c9bed5dce432b4b8cadd458a93d657e56595ee69
Ira Miller <public@iramiller.com>
2018-07-29 11:38:52 -0500

    fix initial supply

--- contracts/EGULD.sol
+++ contracts/EGULD.sol
@@ -6,7 +6,7 @@ contract EGULD is BurnableToken {
     string public name = 'GULD ERC20';
     string public symbol = 'GULD';
     uint public decimals = 8;
-    uint public INITIAL_SUPPLY = 100000;
+    uint public INITIAL_SUPPLY = 10000000000000;
 
     constructor () {
         totalSupply = INITIAL_SUPPLY;
